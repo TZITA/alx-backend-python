ac7a9e846f1cdd398685d224703f433dc344519a
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-07-06 15:22:42 +0300

    Init commit

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Type-Annotated Add function """
+import typing
+
+
+def add(a: float, b: float) -> float:
+    """
+    Type-annotated function to add two float numbers.
+    """
+    return a + b
