1499d9ece5b887c89e627cf8f9184a14073dc26c
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-07-06 15:41:03 +0300

    floor func

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" A type-annotated function floor """
+
+
+def floor(n: float) -> int:
+    """
+    A type-annotated function that returns the floor of a float.
+    """
+    return int(n)
