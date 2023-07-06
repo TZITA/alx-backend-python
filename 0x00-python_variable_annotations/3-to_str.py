93b812f096aac9c8dc3d914c52c9c6693bbd0bfb
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-07-06 15:45:33 +0300

    to_str func

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" A type-annotated to_str function """
+
+
+def to_str(n: float) -> str:
+    """
+    A type-annotated function to covert float to string.
+    """
+    return str(n)
