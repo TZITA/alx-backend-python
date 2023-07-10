813bb501364d2915121951b3594a2e4941d48c5d
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-07-10 19:15:03 +0300

    annotattion of Task object

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 """ Asyncio Tasks """
 import asyncio
-from typing import Awaitable
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int):
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """ Returns asyncio.task of the wait_random() asyn function"""
-    task: Task = asyncio.create_task(wait_random(max_delay))
+    task = asyncio.create_task(wait_random(max_delay))
     return task
