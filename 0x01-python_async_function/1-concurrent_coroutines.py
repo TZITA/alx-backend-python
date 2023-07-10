dfe36c6918e909a4dc8717d6f57f0256620e3335
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-07-10 18:13:46 +0300

    sorted list of async.gather

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -12,4 +12,5 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     Async function that returns list of concurrently executed wait_random()
     """
     coroutines = [wait_random(max_delay) for _ in range(n)]
-    return await asyncio.gather(*coroutines)
+    sorted_res = sorted(await asyncio.gather(*coroutines))
+    return sorted_res
