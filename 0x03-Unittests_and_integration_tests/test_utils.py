afc3eec8ce8009cb922db53d920d235ed22692d5
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-07-31 22:49:36 +0300

    task 2 het_json test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 """ Task 0 """
 import unittest
+import requests
+from unittest.mock import patch, Mock
 from parameterized import parameterized
-from typing import Mapping, Sequence, Union
+from typing import Mapping, Sequence, Union, Dict
 
 
 access_nested_map = __import__("utils").access_nested_map
+get_json = __import__("utils").get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -33,3 +36,18 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(type(exp_exc)) as ex:
             access_nested_map(nested_map, path)
         self.assertEqual(str(ex.exception), str(exp_exc))
+
+
+class TestGetJson(unittest.TestCase):
+    """ TestGetJson test class """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url: str, test_payload: Dict):
+        """test_get_json"""
+        attributes = {"json.return_value": test_payload}
+        with patch("requests.get", return_value=Mock(**attributes)) as req:
+            self.assertEqual(get_json(test_url), test_payload)
+            req.assert_called_once_with(test_url)
