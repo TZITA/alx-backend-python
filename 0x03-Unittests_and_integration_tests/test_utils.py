9316e16add1f7d4a9dc7ed684d20bd729945e4c2
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-07-31 23:28:32 +0300

    task 3 memoize

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,10 +5,7 @@ import requests
 from unittest.mock import patch, Mock
 from parameterized import parameterized
 from typing import Mapping, Sequence, Union, Dict
-
-
-access_nested_map = __import__("utils").access_nested_map
-get_json = __import__("utils").get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -51,3 +48,25 @@ class TestGetJson(unittest.TestCase):
         with patch("requests.get", return_value=Mock(**attributes)) as req:
             self.assertEqual(get_json(test_url), test_payload)
             req.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """TestMemoize test class"""
+    def test_memoize(self):
+        """ test_memoize """
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, "a_method",
+                          return_value=lambda: 42)as mem:
+            test_cls = TestClass()
+
+            self.assertEqual(test_cls.a_property(), 42)
+            self.assertEqual(test_cls.a_property(), 42)
+
+            mem.assert_called_once()
